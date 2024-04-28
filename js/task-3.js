4cc707ecf586db4519848ddcfb4e94fa10af4992
Vova_Kravchyk <158581789+Legionchik@users.noreply.github.com>
2024-04-28 20:12:03 +0200

    Update task-3.js
    redaction totalWidth

--- js/task-3.js
+++ js/task-3.js
@@ -4,7 +4,7 @@
     let contentWidth = parseFloat(content);
     let paddingWidth = parseFloat(padding);
     let borderWidth = parseFloat(border);
-    let totalWidth = contentWidth + paddingWidth + borderWidth;
+    let totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
     return totalWidth;
 }
 
